199f59b2ba1697da9526c1380d4e4b96635f7295
Hsu-ping <56856410+Hsu-ping@users.noreply.github.com>
2022-06-18 13:59:32 +0800

    Rename good.c to card.c

--- card.c
+++ card.c
@@ -28,8 +28,8 @@
 	void print(card *set); 
 	int FindCard(card *set,char *input);
 	card *gettail(card *from);
-		//©âµP°ï = deck
-		//±óµP°ï = deadwood 
+		//æŠ½ç‰Œå † = deck
+		//æ£„ç‰Œå † = deadwood 
 	int main(){
 		card *HandCard[4];
 		for(int i=0;i<4;++i){
@@ -70,7 +70,7 @@
 		return -1;
 	}
 	
-	void print(card *set){			//½Ğ¶ñheader 
+	void print(card *set){			//è«‹å¡«header 
 		card *pointer = set;
 		if(pointer->next == NULL){
 			printf("empty\n");
@@ -80,7 +80,7 @@
 			printf("%15s %7s %2d\n",pointer->name,pointer->suit,pointer->number);
 		}
 	}
-	void Move1Card(card *to,card *from,int number){			//µP°ï¤£¯à¬°ªÅ!!! 
+	void Move1Card(card *to,card *from,int number){			//ç‰Œå †ä¸èƒ½ç‚ºç©º!!! 
 		card *pointer = from;
 		for(int i=0;i<number-1;i++){
 			pointer = pointer->next;
